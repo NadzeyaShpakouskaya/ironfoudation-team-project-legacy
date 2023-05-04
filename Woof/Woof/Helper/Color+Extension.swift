6f7c7bd46797faf90ddbeb20cfafe975f19a56fb
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-05-04 15:08:58 +0400

    Create Color extension for App scheme

---
+++ Woof/Woof/Helper/Color+Extension.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+/// Describes the color scheme using in the application
+extension Color {
+    static let whiteCustom = Color(red: 251 / 255, green: 251 / 255, blue: 251 / 255)
+    static let grayLight = Color(red: 239 / 255, green: 232 / 255, blue: 232 / 255)
+    static let grayDark = Color(red: 126 / 255, green: 126 / 255, blue: 126 / 255)
+    static let purpleLight = Color(red: 195 / 255, green: 164 / 255, blue: 195 / 255)
+    static let purpleDark = Color(red: 116 / 255, green: 75 / 255, blue: 116 / 255)
+}
