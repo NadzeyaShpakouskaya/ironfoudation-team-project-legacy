0bfdf17eb34e050cf82fcf569bb1f82c26cb1ba7
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-05-05 11:01:53 +0200

    typo fixing

--- Woof/Woof/Model/PetOwner.swift
+++ Woof/Woof/Model/PetOwner.swift
@@ -1,28 +1,29 @@
 import Foundation
 
 /**
-A struct representing a pet owner.
-This struct conforms to the Identifiable and Codable protocols, allowing it to be easily used in SwiftUI and encoded/decoded from JSON.
-*/
+ A struct representing a pet owner.
+ This struct conforms to the Identifiable and Codable protocols, allowing it
+ to be easily used in SwiftUI and encoded/decoded from JSON.
+ */
 struct PetOwner: Identifiable, Codable {
-    ///Uniquely identifies the pet owner.
-    var id: UUID
-    
-    ///The first name of the pet owner.
+    /// Uniquely identifies the pet owner.
+    let id: UUID
+
+    /// The first name of the pet owner.
     var name: String
-    
-    ///The last name of the pet owner.
+
+    /// The last name of the pet owner.
     var surname: String
-    
-    ///The phone number of the pet owner.
+
+    /// The phone number of the pet owner.
     var phone: String
-    
-    ///The optionl URL of the pet owner's avatar image.
+
+    /// The optionl URL of the pet owner's avatar image.
     var avatarUrl: URL?
-    
-    ///The address of the pet owner.
+
+    /// The address of the pet owner.
     var address: String
-    
-    ///The rating of the pet owner from 0 to 5.
-    var Rating: Double
+
+    /// The rating of the pet owner from 0 to 5.
+    var rating: Double
 }
