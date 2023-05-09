c905f974451802c5825526ed8ac86e53583532d8
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-05-09 16:22:44 +0400

    Add doc comments for sitter card view model

--- Woof/Woof/View Model/SitterCardViewModel.swift
+++ Woof/Woof/View Model/SitterCardViewModel.swift
@@ -1,21 +1,31 @@
 import Foundation
 
-/**
-
- */
+/// The view model for a sitter card, responsible for preparing and providing data for the view.
 final class SitterCardViewModel: ObservableObject {
+    /// The full name of the sitter.
     @Published var fullName = ""
+
+    /// The additional information about the sitter.
     @Published var bio = ""
+
+    /// The rating of the sitter, on a scale from 0 to 5.
     @Published var rating = 0
+
+    /// The price per hour charged by the sitter.
     @Published var price = 0.0
+
+    /// The URL of the sitter's avatar image.
     @Published var imageURL: URL?
 
-    private var sitter: Sitter
-
+    /// Initializes a new instance of the sitter card view model with the provided sitter.
+    ///
+    /// - Parameter sitter: The sitter for which the view model is created.
     init(sitter: Sitter) {
         self.sitter = sitter
         prepareDataForView()
     }
+
+    private var sitter: Sitter
 
     private func prepareDataForView() {
         fullName = getFormattedNameFor(surname: sitter.surname, name: sitter.name)
