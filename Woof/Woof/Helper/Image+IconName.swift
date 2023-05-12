5c291204344f8f178ebcaa633d3cbcf9e6bfe929
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-05-12 09:30:43 +0400

    Add image names for icons used in sitter card view

--- Woof/Woof/Helper/Image+IconName.swift
+++ Woof/Woof/Helper/Image+IconName.swift
@@ -11,5 +11,11 @@ extension Image {
         static let walkingsTab = "pawprint"
         /// The icon used for the "Profile" tab.
         static let profileTab = "person.text.rectangle"
+        /// The icon used for the 'Avatar' placeholder.
+        static let avatarPlaceholder = "person.circle"
+        /// The icon used for the filled star image.
+        static let filledStar = "star.fill"
+        /// The icon used for the unfilled star image.
+        static let unfilledStar = "star"
     }
 }
