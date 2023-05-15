0965a4b092baabec15b59d39616af5d0055bea63
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-05-15 22:41:45 +0400

    Add list of dummy sitters to Sitter model, add dummy to view model

--- Woof/Woof/Model/Sitter/Sitter+Dummy.swift
+++ Woof/Woof/Model/Sitter/Sitter+Dummy.swift
@@ -20,5 +20,20 @@ extension Sitter {
             rating: 4.8,
             pricePerHour: 40.25
         )
+
+        /// The instance of dummy sitter list to use in preview of views.
+        static let sitters = [
+            // swiftlint:disable line_length
+            Sitter(id: UUID(), name: "Jane", surname: "Smith", phone: "0987654321", avatarUrl: Bundle.main.url(forResource: "EmilyDoeAvatar", withExtension: "jpg"), bio: "Certified dog trainer. Enjoys playing fetch and teaching new tricks.", rating: 4.8, pricePerHour: 20.0),
+            Sitter(id: UUID(), name: "John", surname: "Alison", phone: "1234567890", avatarUrl: URL(string: "https://example.com/avatar1.jpg"), bio: "Experienced dog lover. Loves taking dogs for long walks in the park.", rating: 4.5, pricePerHour: 15.0),
+            Sitter(id: UUID(), name: "Emily", surname: "Johnson", phone: "9876543210", avatarUrl: URL(string: "https://example.com/avatar3.jpg"), bio: "Passionate about caring for dogs. Enjoys spending time outdoors with furry friends.", rating: 4.9, pricePerHour: 25.0),
+            Sitter(id: UUID(), name: "Michael", surname: "Brown", phone: "0123456789", avatarUrl: URL(string: "https://example.com/avatar4.jpg"), bio: "Experienced pet sitter. Provides a safe and comfortable environment for dogs.", rating: 4.7, pricePerHour: 18.0),
+            Sitter(id: UUID(), name: "Olivia", surname: "Davis", phone: "8765432109", avatarUrl: URL(string: "https://example.com/avatar5.jpg"), bio: "Loves dogs of all sizes and breeds. Enjoys exploring new walking trails.", rating: 4.6, pricePerHour: 17.0),
+            Sitter(id: UUID(), name: "William", surname: "Wilson", phone: "5432109876", avatarUrl: URL(string: "https://example.com/avatar6.jpg"), bio: "Dog enthusiast with a background in pet care. Provides personalized attention to every furry client.", rating: 4.7, pricePerHour: 19.0),
+            Sitter(id: UUID(), name: "Sophia", surname: "Taylor", phone: "9012345678", avatarUrl: URL(string: "https://example.com/avatar7.jpg"), bio: "Experienced in handling energetic dogs. Creates fun and engaging activities for furry pals.", rating: 4.8, pricePerHour: 22.0),
+            Sitter(id: UUID(), name: "James", surname: "Anderson", phone: "6789012345", avatarUrl: URL(string: "https://example.com/avatar8.jpg"), bio: "Professional dog walker with a passion for canine fitness. Customizes exercise routines based on individual needs.", rating: 4.7, pricePerHour: 21.0),
+            Sitter(id: UUID(), name: "Ava", surname: "Moore", phone: "4567890123", avatarUrl: URL(string: "https://example.com/avatar9.jpg"), bio: "Compassionate dog lover. Provides a loving and caring environment for furry companions.", rating: 4.5, pricePerHour: 16.0),
+            // swiftlint:enable line_length
+        ]
     }
 }
--- Woof/Woof/ViewModel/SitterList:ViewModel.swift
+++ Woof/Woof/ViewModel/SitterList:ViewModel.swift
@@ -1,8 +1,13 @@
-//
-//  SitterList:ViewModel.swift
-//  Woof
-//
-//  Created by Nadzeya Shpakouskaya on 15/05/2023.
-//
-
 import Foundation
+
+/// Responsible for preparing and providing data for the `SitterListView`.
+final class SitterListViewModel: ObservableObject {
+    /// The sequence of sitters to display in view.
+    @Published var sitters: [Sitter] = []
+
+    /// Initializes a new instance of the `SitterListViewModel`
+    init() {
+        // stub to load dummy sitter list
+        sitters = Sitter.Dummy.sitters
+    }
+}
