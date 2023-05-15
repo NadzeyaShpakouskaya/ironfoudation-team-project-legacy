f53261d110d4d1ce07935bbc49d39585bcb1388e
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-05-15 22:38:44 +0400

    Add new files: SitterListView,  SitterListViewModel, reorganize file structure

---
+++ Woof/Woof/View/SitterListView/SitterListView.swift
@@ -0,0 +1,20 @@
+//
+//  SitterListView.swift
+//  Woof
+//
+//  Created by Nadzeya Shpakouskaya on 15/05/2023.
+//
+
+import SwiftUI
+
+struct SitterListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SitterListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SitterListView()
+    }
+}
---
+++ Woof/Woof/ViewModel/SitterList:ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SitterList:ViewModel.swift
+//  Woof
+//
+//  Created by Nadzeya Shpakouskaya on 15/05/2023.
+//
+
+import Foundation
