780c2627df536e437f2de42a24b4c82edff6c285
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-05-15 09:20:33 +0400

    FontStyle: update the doc comments

--- Woof/Woof/Helper/AppStyle/AppStyle+FontStyle.swift
+++ Woof/Woof/Helper/AppStyle/AppStyle+FontStyle.swift
@@ -1,15 +1,16 @@
 import SwiftUI
 
 extension AppStyle {
-    /// Provides the font sizes used within the application.
+    /// Provides the font style using within the application.
     enum FontStyle {
-        /// Size of font uses for titles and headings.
+        /// Font style using for titles and headings.
         case heading
-        /// Size of font uses as default font size.
+        /// Font style using as default font size.
         case body
-        /// Size of font uses for additional elements.
+        /// Font style using for additional elements.
         case footnote
 
+        /// The font size setting for font style.
         var size: CGFloat {
             switch self {
             case .heading:
