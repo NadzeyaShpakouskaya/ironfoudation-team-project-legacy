d582234f51622d991456ba2727687b95b7c37ca7
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-05-15 13:58:18 +0200

    Creating file struct for Owner view

---
+++ Woof/Woof/View/OwnerCardView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct OwnerCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OwnerCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OwnerCardView()
+    }
+}
