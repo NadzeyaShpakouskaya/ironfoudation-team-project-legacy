197bb1d59c7fb2e003cca98b3b57c5d66ea423da
Julya Kmita <julyakmita@Julyas-MacBook-Air.local>
2023-05-17 17:10:06 +0800

    Add file Woof/Woof/View/DetailedPetSitterView/RaitingView.swift

--- Woof/Woof/View/DetailedPetSitterView/RaitingView.swift
+++ Woof/Woof/View/DetailedPetSitterView/RaitingView.swift
@@ -1,20 +1,49 @@
-//
-//  RaitingView.swift
-//  Woof
-//
-//  Created by Julya Kmita on 17/5/23.
-//
-
 import SwiftUI
 
-struct RaitingView: View {
+/// A view displaying a rating of the person.
+struct RatingView: View {
+    /// The rating of the sitter.
+    @Binding var rating: Int
+    /// Maximum rating value
+    var maximumRating = 5
+
+    /// Not highlited image.
+    var offImage: Image?
+    /// Highlited image.
+    var onImage = Image(systemName: "star.fill")
+    /// Color used for a filled raiting image.
+
+    var offColor = Color.App.grayDark
+    /// Color used for an unfilled raiting image.
+    var onColor = Color.App.purpleDark
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if rating == 0 {
+                Text("not rated")
+                    .font(Font.system(size: AppStyle.FontStyle.footnote.size))
+            } else {
+                ForEach(1..<maximumRating + 1, id: \.self) { number in image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+                    .onTapGesture {
+                        rating = number
+                    }
+                }
+            }
+        }
+    }
+
+    private func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
     }
 }
 
-struct RaitingView_Previews: PreviewProvider {
+struct RatingView_Previews: PreviewProvider {
     static var previews: some View {
-        RaitingView()
+        RatingView(rating: .constant(3))
     }
 }
