fbb6898372021d39f7931e6bc77cfbbab80f7960
Julya Kmita <julyakmita@Julyas-MacBook-Air.local>
2023-05-17 17:14:04 +0800

    Add file Woof/Woof/View/DetailedPetSitterView/ContactDelailsSectionView.swift

--- Woof/Woof/View/DetailedPetSitterView/ContactDelailsSectionView.swift
+++ Woof/Woof/View/DetailedPetSitterView/ContactDelailsSectionView.swift
@@ -1,20 +1,28 @@
-//
-//  ContactDelailsView.swift
-//  Woof
-//
-//  Created by Julya Kmita on 17/5/23.
-//
-
 import SwiftUI
 
-struct ContactDelailsView: View {
+/// A view displaying a section for the detailed sitter's information.
+struct ContactDelailsSectionView: View {
+    /// The name of an icon.
+    let image: String
+    /// The informational  text to be displayed.
+    let message: String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: AppStyle.UIElementConstant.cornerRadius)
+            .padding(.horizontal)
+            .frame(height: AppStyle.UIElementConstant.frameContactDetailsSectionHeight)
+            .overlay(HStack {
+                Image(systemName: image)
+                Text(message)
+                    .foregroundColor(Color.App.grayDark)
+                    .fontWeight(.bold)
+            }.foregroundColor(Color.App.purpleDark))
+            .foregroundColor(Color.App.white)
     }
 }
 
-struct ContactDelailsView_Previews: PreviewProvider {
+struct ContactDetailsSectionView_Previews: PreviewProvider {
     static var previews: some View {
-        ContactDelailsView()
+        ContactDelailsSectionView(image: Image.IconName.fullName, message: "Full Name")
     }
 }
