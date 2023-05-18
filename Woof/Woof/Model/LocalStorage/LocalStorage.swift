8e442ce3357026cfbb7f1b837a75b438853c58a0
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-05-18 12:35:24 +0200

    Adding tests for localstorage

--- Woof/Woof/Model/LocalStorage/LocalStorage.swift
+++ Woof/Woof/Model/LocalStorage/LocalStorage.swift
@@ -3,31 +3,31 @@ import Foundation
 /// A utility model for saving and loading data from user defaults.
 class LocalStorage {
     /**
-     Saves the given value to the user defaults for the specified key.
-     
-     - Parameters:
-       - value: The value to be saved. Must be encodable.
-       - key: The key under which to save the value.
-     
-     - Throws: An error of type `LocalStorageError.encodeError` if encoding the value fails.
-    */
-    func save<T: Encodable>(value: T, for key: String) throws {
+      Saves the given value to the user defaults for the specified key.
+
+      - Parameters:
+        - value: The value to be saved. Must be encodable.
+        - key: The key under which to save the value.
+
+      - Throws: An error of type `LocalStorageError.encodeError` if encoding the value fails.
+     */
+    func save(value: some Encodable, for key: String) throws {
         let data = try CodableHelper.encode(value)
         UserDefaults.standard.set(data, forKey: key)
     }
-    
+
     /**
-     Loads the value from the user defaults for the specified key and returns it.
-     
-     - Parameters:
-       - key: The key for which to load the value.
-     
-     - Returns: The loaded value of type `T`.
-     
-     - Throws:
-       - An error of type `LocalStorageError.noDataForKey` if no data is found for the specified key.
-       - An error of type `LocalStorageError.decodeError` if decoding the data fails.
-    */
+      Loads the value from the user defaults for the specified key and returns it.
+
+      - Parameters:
+        - key: The key for which to load the value.
+
+      - Returns: The loaded value of type `T`.
+
+      - Throws:
+        - An error of type `LocalStorageError.noDataForKey` if no data is found for the specified key.
+        - An error of type `LocalStorageError.decodeError` if decoding the data fails.
+     */
     func loadValue<T: Decodable>(key: String) throws -> T {
         guard let data = UserDefaults.standard.data(forKey: key) else {
             throw LocalStorageError.noDataForKey
@@ -37,7 +37,7 @@ class LocalStorage {
 
         return result
     }
-    
+
     /// Possible keys used for storing data in user defaults.
     enum Key: String {
         /// Key representing the data of the owner.
---
+++ Woof/WoofTests/Model/LocalStorage/LocalStorageSaveLoadTests.swift
@@ -0,0 +1,82 @@
+import XCTest
+
+final class LocalStorageSaveTests: XCTestCase {
+    var localStorage: LocalStorage!
+
+    override func setUp() {
+        let storage = LocalStorage()
+        localStorage = storage
+
+        UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+    }
+
+    override func tearDown() {
+        localStorage = nil
+        super.tearDown()
+    }
+
+    func testSaveAndLoadValue() {
+        do {
+            // given
+            let name = "John Brown"
+            try localStorage.save(value: name, for: LocalStorage.Key.owner.rawValue)
+
+            // when
+            let loadedName: String = try localStorage.loadValue(key: LocalStorage.Key.owner.rawValue)
+
+            // then
+            XCTAssertEqual(loadedName, name)
+        } catch {
+            XCTFail("Failed to save or load value: \(error)")
+        }
+    }
+
+    func testLoadValueWithNoDataForKey() {
+        do {
+            // given
+
+            // when
+            let _: String = try localStorage.loadValue(key: LocalStorage.Key.owner.rawValue)
+
+            XCTFail("No error was thrown for loading a non-existent value.")
+        } catch LocalStorageError.noDataForKey {
+            // then
+            XCTAssert(true)
+        } catch {
+            XCTFail("Unexpected error thrown: \(error)")
+        }
+    }
+
+    func testEncode() {
+        do {
+            // given
+            let name = "John Doe"
+
+            // when
+            let encodedData = try CodableHelper.encode(name)
+
+            XCTAssertNotNil(encodedData)
+            let decodedName = try JSONDecoder().decode(String.self, from: encodedData)
+            // then
+            XCTAssertEqual(decodedName, name)
+        } catch {
+            XCTFail("Failed to encode: \(error)")
+        }
+    }
+
+    func testDecode() {
+        do {
+            // given
+            let name = "John Doe"
+            let encodedData = try JSONEncoder().encode(name)
+
+            // when
+            let decodedName: String = try CodableHelper.decode(encodedData, as: String.self)
+
+            // then
+            XCTAssertEqual(decodedName, name)
+        } catch {
+            XCTFail("Failed to decode: \(error)")
+        }
+    }
+}
