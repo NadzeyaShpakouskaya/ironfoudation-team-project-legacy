4dffa8a9824f838e4e5cf5a93d484497e791966a
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-05-18 17:08:08 +0200

    Deleting tests for CodableHelper

--- Woof/WoofTests/Model/LocalStorage/LocalStorageSaveLoadTests.swift
+++ Woof/WoofTests/Model/LocalStorage/LocalStorageSaveLoadTests.swift
@@ -46,37 +46,4 @@ final class LocalStorageSaveTests: XCTestCase {
             XCTFail("Unexpected error thrown: \(error)")
         }
     }
-
-    func testEncode() {
-        do {
-            // given
-            let name = "John Brown"
-
-            // when
-            let encodedData = try CodableHelper.encode(name)
-
-            XCTAssertNotNil(encodedData)
-            let decodedName = try JSONDecoder().decode(String.self, from: encodedData)
-            // then
-            XCTAssertEqual(decodedName, name)
-        } catch {
-            XCTFail("Failed to encode: \(error)")
-        }
-    }
-    
-    func testDecode() {
-        do {
-            // given
-            let name = "John Brown"
-            let encodedData = try JSONEncoder().encode(name)
-            
-            // when
-            let decodedName: String = try CodableHelper.decode(encodedData, as: String.self)
-            
-            // then
-            XCTAssertEqual(decodedName, name)
-        } catch {
-            XCTFail("Failed to decode: \(error)")
-        }
-    }
 }
