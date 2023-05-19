404d9f28726234fa2f35c4b68c8b2ebfa6da2698
Julya Kmita <>
2023-05-19 16:41:34 +0800

    Add modified file Woof/Woof.xcodeproj/project.pbxproj

--- Woof/Woof/View/DetailedPetSitterView/RaitingView.swift
+++ Woof/Woof/View/DetailedPetSitterView/RaitingView.swift
@@ -1,49 +1,30 @@
 import SwiftUI
 
 /// A view displaying a rating of the person.
-struct RatingView: View {
-    /// The rating of the sitter.
-    @Binding var rating: Int
-    /// Maximum rating value
-    var maximumRating = 5
-
-    /// Not highlited image.
-    var offImage: Image?
-    /// Highlited image.
-    var onImage = Image(systemName: "star.fill")
-
-    /// Color used for a filled raiting image.
-    var offColor = Color.App.grayDark
-    /// Color used for an unfilled raiting image.
-    var onColor = Color.App.purpleDark
+struct RaitingView: View {
+    /// The rating of the person.
+    let rating: Int
 
     var body: some View {
-        HStack {
-            if rating == 0 {
-                Text("not rated")
-                    .font(Font.system(size: AppStyle.FontStyle.footnote.size))
-            } else {
-                ForEach(1..<maximumRating + 1, id: \.self) { number in image(for: number)
-                    .foregroundColor(number > rating ? offColor : onColor)
-                    .onTapGesture {
-                        rating = number
+        VStack(alignment: .leading) {
+            Group {
+                if rating == 0 {
+                    Text("not rated")
+                        .font(Font.system(size: AppStyle.FontStyle.footnote.size))
+                } else {
+                    HStack(spacing: 0) {
+                        ForEach(0..<rating, id: \.self) { _ in
+                            Image(systemName: Image.IconName.filledStar)
+                        }
                     }
                 }
-            }
-        }
-    }
-
-    private func image(for number: Int) -> Image {
-        if number > rating {
-            return offImage ?? onImage
-        } else {
-            return onImage
+            }.foregroundColor(Color.App.purpleDark)
         }
     }
 }
 
-struct RatingView_Previews: PreviewProvider {
+struct Raiting_Previews: PreviewProvider {
     static var previews: some View {
-        RatingView(rating: .constant(3))
+        RaitingView(rating: 3)
     }
 }
