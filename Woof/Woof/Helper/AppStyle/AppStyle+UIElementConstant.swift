7f255eb202d34257bb984f59340028f5abdf4c45
Julya Kmita <>
2023-05-19 21:27:52 +0800

    Removed unnecessary constant

--- Woof/Woof/Helper/AppStyle/AppStyle+UIElementConstant.swift
+++ Woof/Woof/Helper/AppStyle/AppStyle+UIElementConstant.swift
@@ -12,9 +12,6 @@ extension AppStyle {
         /// The minimum padding size.
         static let minPadding = 4.0
 
-        /// The height of the ContactDetailsSectionHeight.
-        static let frameContactDetailsSectionHeight = 50.0
-
         /// The default side size of an avatar image.
         static let avatarSideSize = 80.0
     }
