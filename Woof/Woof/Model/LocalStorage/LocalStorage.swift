e8afc11f570ad1ec7fc85ceb5101d920075d9cb3
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-05-19 11:35:58 +0200

    Deleting codablehelper

--- Woof/Woof/Model/LocalStorage/LocalStorage.swift
+++ Woof/Woof/Model/LocalStorage/LocalStorage.swift
@@ -3,37 +3,49 @@ import Foundation
 /// A utility model for saving and loading data from user defaults.
 class LocalStorage {
     /**
-      Saves the given value to the user defaults for the specified key.
+      Saves an encodable value to the UserDefaults with the specified key.
 
       - Parameters:
-        - value: The value to be saved. Must be encodable.
-        - key: The key under which to save the value.
+         - value: The value to be encoded and saved.
+         - key: The key under which the encoded value will be stored.
 
-      - Throws: An error of type `LocalStorageError.encodeError` if encoding the value fails.
+      - Throws: An error of type `LocalStorageError.encodeError` if the encoding fails.
+
+      This function encodes the `value` using `JSONEncoder` and stores the encoded data in the
+     UserDefaults with the specified `key`. If the encoding fails, an error is thrown.
      */
     func save(value: some Encodable, for key: String) throws {
-        let data = try CodableHelper.encode(value)
+        guard let data = try? JSONEncoder().encode(value) else {
+            throw LocalStorageError.encodeError
+        }
         UserDefaults.standard.set(data, forKey: key)
     }
 
     /**
-      Loads the value from the user defaults for the specified key and returns it.
+      Loads a decodable value from UserDefaults using the specified key.
 
       - Parameters:
-        - key: The key for which to load the value.
+         - key: The key associated with the value in UserDefaults.
 
-      - Returns: The loaded value of type `T`.
+      - Returns: The decoded value of generic type `T` if it exists.
 
-      - Throws:
-        - An error of type `LocalStorageError.noDataForKey` if no data is found for the specified key.
-        - An error of type `LocalStorageError.decodeError` if decoding the data fails.
+      - Throws: An error of type `LocalStorageError.noDataForKey` if no data exists for the specified key,
+      or an error of type `LocalStorageError.decodeError` if the decoding fails.
+
+      This function retrieves the data associated with the specified `key` from UserDefaults.
+      If the data exists, it attempts to decode it as type `T` using `JSONDecoder`.
+      If the decoding is successful, the decoded value is returned.
+      If no data exists for the specified key, an error is thrown indicating the absence of data.
+      If the decoding fails, an error is thrown.
      */
     func loadValue<T: Decodable>(key: String) throws -> T {
         guard let data = UserDefaults.standard.data(forKey: key) else {
             throw LocalStorageError.noDataForKey
         }
 
-        let result = try CodableHelper.decode(data, as: T.self)
+        guard let result = try? JSONDecoder().decode(T.self, from: data) else {
+            throw LocalStorageError.decodeError
+        }
 
         return result
     }
