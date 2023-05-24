e9c9518ec70d7ead7287d62c025e32d68df63821
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-05-24 12:11:19 +0200

    Using own userdefaults instance for storage data, reworking documentation

--- Woof/Woof/Model/LocalStorage/LocalStorage.swift
+++ Woof/Woof/Model/LocalStorage/LocalStorage.swift
@@ -1,40 +1,43 @@
 import Foundation
 
-/// A persistent data storage mechanism, allowing you to store key-value pairs
-/// that remain accessible across multiple app launches.
+/// A persistent storage designed for managing a collection of values.
+/// These values are stored and retrieved using a key that uniquely identifies the value.
+/// The key is used to find the data within the storage.
 class LocalStorage {
+    private let localStorage = UserDefaults(suiteName: "woofLocalStorage")
+
     /**
-      Saves a value to the data storage with the specified key.
+      Saves the value associated with the specified key.
 
       - Parameters:
          - value: The value to be encoded and saved.
          - key: The key under which the encoded value will be stored.
      */
     func save(value: Data, for key: String) {
-        UserDefaults.standard.set(value, forKey: key)
+        localStorage?.set(value, forKey: key)
     }
 
     /**
-      Loads a value from data storage using the specified key.
+      Retrieves the value associated with the given key.
 
       - Parameters:
          - key: The key associated with the value in data storage.
-         - type: The expected data type of the value from the storage.
 
-      - Returns: The decoded value of generic type `T` if it exists.
-     // TODO: returns nil, then///
+      - Returns: The value associated with the given key.
+     If there's no value in the storage associated with the given key returns `nil`.
      */
     func loadValue(for key: String) -> Data? {
-        UserDefaults.standard.data(forKey: key)
+        localStorage?.data(forKey: key)
     }
 
     /**
       Deletes the value from data storage associated with the specified key.
+      If non-existing key, the data storage will remain unchanged, and there will be no error or exception thrown.
 
       - Parameters:
          - key: The key associated with the value in data storage.
      */
     func deleteValue(forKey key: String) {
-        UserDefaults.standard.removeObject(forKey: key)
+        localStorage?.removeObject(forKey: key)
     }
 }
