54f306c21e5f29d59cde1b7aeae2fda8710cf360
Dersim Davaod <dersim-davaod@Dersims-MacBook-Pro.local>
2023-05-29 10:38:27 +0300

    Implement KeyValueStorageInitTests.

--- Woof/WoofTests/Model/KeyValueStorage/KeyValueStorageInitTests.swift
+++ Woof/WoofTests/Model/KeyValueStorage/KeyValueStorageInitTests.swift
@@ -1,19 +1,44 @@
 import XCTest
 
 final class KeyValueStorageInitTests: XCTestCase {
-    private var storage = KeyValueStorage.test
-
-    override func setUp() async throws {
-        try await super.setUp()
-        storage.deleteDataForAllTestKeys()
+    func testInitMethodExistsInAPI() {
+        let _ = KeyValueStorage("Test")
     }
 
-    override func tearDown() async throws {
-        storage.deleteDataForAllTestKeys()
-        try await super.tearDown()
+    func testLoadMethodRetrievesTheDataSavedUsingTheDifferentReferenceButTheSameName() {
+        // given
+        let storageName = "test"
+        let key = KeyValueStorage.TestKey.keyA.rawValue
+        let dataSavedInA = KeyValueStorage.TestData.dataOneByte
+
+        let storageA = KeyValueStorage(storageName)
+        storageA.save(dataSavedInA, for: key)
+
+        // when
+        let storageB = KeyValueStorage(storageName)
+        let dataRetrievedFromB = storageB.loadData(for: key)
+
+        // then
+        XCTAssertEqual(dataRetrievedFromB, dataSavedInA)
     }
 
-    func testInitMethodCreates() {
-        // TODO:
+    func testLoadMethodDoesNotRetrieveTheDataRemovedUsingTheDifferentReferenceButTheSameName() {
+        // given
+        let storageName = "test"
+        let key = KeyValueStorage.TestKey.keyA.rawValue
+        let dataSavedInA = KeyValueStorage.TestData.dataOneByte
+
+        let storageA = KeyValueStorage(storageName)
+        storageA.save(dataSavedInA, for: key)
+
+        // when
+        let storageB = KeyValueStorage(storageName)
+        storageB.deleteData(for: key)
+
+        let storageC = KeyValueStorage(storageName)
+        let dataRetrievedFromC = storageC.loadData(for: key)
+
+        // then
+        XCTAssertNil(dataRetrievedFromC)
     }
 }
