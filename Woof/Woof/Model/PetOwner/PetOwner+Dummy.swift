348abdf8bc35d7866270d35b3cba8ccfe83095b5
Julya Kmita <>
2023-05-30 21:55:25 +0800

    Modified file Woof/Woof/Model/PetOwner/PetOwner+Dummy.swift

--- Woof/Woof/Model/PetOwner/PetOwner+Dummy.swift
+++ Woof/Woof/Model/PetOwner/PetOwner+Dummy.swift
@@ -1,8 +1,14 @@
-//
-//  PetOwner+Dummy.swift
-//  Woof
-//
-//  Created by Julya Kmita on 29/5/23.
-//
-
 import Foundation
+extension Owner {
+    enum Dummy {
+        static let laraCroft = Owner(
+            id: UUID(),
+            name: "Lara",
+            surname: "Croft",
+            phone: "6734828993487",
+            avatarUrl: Bundle.main.url(forResource: "EmilyDoeAvatar", withExtension: "jpg"),
+            address: "My town, Happiness str. 24-67",
+            rating: 4.8
+        )
+    }
+}
