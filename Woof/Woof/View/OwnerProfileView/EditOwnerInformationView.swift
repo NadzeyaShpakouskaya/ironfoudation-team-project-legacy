9f8f0ecbaa5c8aae718babd064d64f5850cbdd4d
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-05-30 09:47:34 +0400

    Add EditOwnerInformationView

---
+++ Woof/Woof/View/OwnerProfileView/EditOwnerInformationView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+
+/// A view to edit the owner information.
+struct EditOwnerInformationView: View {
+    /// The name of the owner
+    @Binding var name: String
+
+    /// The surname of the owner
+    @Binding var surname: String
+
+    /// The phone of the owner
+    @Binding var phone: String
+
+    /// The address of the owner
+    @Binding var address: String
+
+    var body: some View {
+        VStack {
+            Text("Edit your information")
+            TextField("Name", text: $name)
+            TextField("Surname", text: $surname)
+            TextField("Phone", text: $phone)
+            TextField("Address", text: $address)
+
+            if name.isEmpty {
+                Text("*Name is a mandatory field")
+            }
+        }
+        .padding()
+        .foregroundColor(.App.purpleDark)
+        .textFieldStyle(.roundedBorder)
+        .background(Color.App.purpleLight)
+        .cornerRadius(AppStyle.UIElementConstant.cornerRadius)
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct EditOwnerInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            EditOwnerInformationView(
+                name: .constant("Emily"),
+                surname: .constant("Thompson"),
+                phone: .constant("527637624567"),
+                address: .constant("My home address")
+            )
+
+            EditOwnerInformationView(
+                name: .constant(""),
+                surname: .constant(""),
+                phone: .constant(""),
+                address: .constant("")
+            )
+        }
+    }
+}
