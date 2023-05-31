b62b71f6e231cc28faa7d8adb9c716420bd1a826
Julya Kmita <>
2023-05-31 12:36:02 +0800

    Add file Woof/Woof/View/ReusableViews/LabelWithTextAndImage.swift

--- Woof/Woof/View/ReusableViews/LabelWithTextAndImage.swift
+++ Woof/Woof/View/ReusableViews/LabelWithTextAndImage.swift
@@ -1,20 +1,25 @@
-//
-//  LabelWithTextAndImage.swift
-//  Woof
-//
-//  Created by Julya Kmita on 31/5/23.
-//
-
 import SwiftUI
 
+/// A view displaying an icon and the portion of text.
 struct LabelWithTextAndImage: View {
+    /// The name of the icon that the label displays.
+    let iconName: String
+
+    /// The text that the label displays.
+    let text: String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Label(text, systemImage: iconName)
+            .font(.system(size: AppStyle.FontStyle.body.size, weight: .bold))
+            .background(Color.App.purpleLight)
+            .foregroundColor(Color.App.white)
     }
 }
 
 struct LabelWithTextAndImage_Previews: PreviewProvider {
     static var previews: some View {
-        LabelWithTextAndImage()
+        LabelWithTextAndImage(
+            iconName: Image.IconName.phone, text: "80965656569"
+        )
     }
 }
