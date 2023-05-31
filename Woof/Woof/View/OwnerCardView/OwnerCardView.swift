1edd1725e644383e5582270c7f46c931f97f08fe
Julya Kmita <>
2023-05-31 12:34:45 +0800

    add file Woof/Woof/View/OwnerCardView/OwnerCardView.swift

--- Woof/Woof/View/OwnerCardView/OwnerCardView.swift
+++ Woof/Woof/View/OwnerCardView/OwnerCardView.swift
@@ -1,21 +1,9 @@
 import SwiftUI
 
-// A view to present the owner information.
+/// A view displaying  information about the pet owner.
 struct OwnerCardView: View {
     /// The view model responsible for providing data to the view.
     @ObservedObject var viewModel: OwnerCardViewModel
-
-//    /// The name of the owner
-//    let fullName: String
-//
-//    /// The phone of the owner
-//    let phone: String
-//
-//    /// The address of the owner
-//    let address: String
-//
-//    /// The URL of the avatar image of the owner.
-//    let avatarUrl: URL?
 
     var body: some View {
         HStack {
@@ -23,25 +11,33 @@ struct OwnerCardView: View {
 
             VStack(alignment: .leading, spacing: 8) {
                 Text(viewModel.fullName)
-                    .font(.system(size: AppStyle.FontStyle.heading.size, weight: .heavy)).lineLimit(1)
-                Label(viewModel.phoneNumber, systemImage: Image.IconName.phone)
-                    .font(.system(size: AppStyle.FontStyle.body.size, weight: .bold))
-
-                Label(viewModel.address, systemImage: Image.IconName.house)
-                    .font(.system(size: AppStyle.FontStyle.body.size, weight: .bold))
+                    .font(
+                        .system(
+                            size: AppStyle.FontStyle.heading.size, weight: .heavy
+                        ))
+                    .lineLimit(1)
+                    .foregroundColor(.App.white)
+                LabelWithTextAndImage(
+                    iconName: Image.IconName.phone,
+                    text: viewModel.phoneNumber
+                )
+                LabelWithTextAndImage(
+                    iconName: Image.IconName.house,
+                    text: viewModel.address
+                )
             }
             Spacer()
         }
         .padding()
         .background(Color.App.purpleLight)
-        .foregroundColor(.App.white)
         .cornerRadius(AppStyle.UIElementConstant.cornerRadius)
         .frame(maxWidth: .infinity)
     }
 
     struct OwnerCardView_Previews: PreviewProvider {
         static var previews: some View {
-            OwnerCardView(viewModel: OwnerCardViewModel(owner: Owner.Dummy.laraCroft))
+            OwnerCardView(
+                viewModel: OwnerCardViewModel(owner: Owner.Dummy.laraCroft))
         }
     }
 }
