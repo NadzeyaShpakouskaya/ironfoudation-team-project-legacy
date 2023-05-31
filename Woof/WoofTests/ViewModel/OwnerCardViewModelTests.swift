18bf8a4d55da90aad96a69adb4143c3c6c4c1e92
Julya Kmita <>
2023-05-31 12:36:55 +0800

    Add file Woof/WoofTests/ViewModel/OwnerCardViewModelTests.swift

--- Woof/WoofTests/ViewModel/OwnerCardViewModelTests.swift
+++ Woof/WoofTests/ViewModel/OwnerCardViewModelTests.swift
@@ -1,8 +1,34 @@
-//
-//  OwnerCardViewModelTests.swift
-//  WoofTests
-//
-//  Created by Julya Kmita on 31/5/23.
-//
+import XCTest
 
-import Foundation
+final class OwnerCardViewModelTests: XCTestCase {
+    func testOwnerCardViewModelExistsInAPI() {
+        let owner = Owner(
+            id: UUID(),
+            name: "Lara",
+            surname: "Croft",
+            phone: "802995349399",
+            address: "My town, Happiness str. 24-67",
+            rating: 4.8
+        )
+
+        _ = OwnerCardViewModel(owner: owner)
+    }
+
+    func testOwnerCardViewModelPropertiesReturnsExpectedValues() {
+        let owner = Owner(
+            id: UUID(),
+            name: "Lara",
+            surname: "Croft",
+            phone: "802995349399",
+            address: "My town, Happiness str. 24-67",
+            rating: 4.8
+        )
+
+        let viewModel = OwnerCardViewModel(owner: owner)
+
+        XCTAssertEqual(viewModel.fullName, "\(owner.name) \(owner.surname)")
+        XCTAssertEqual(viewModel.phoneNumber, owner.phone)
+        XCTAssertEqual(viewModel.address, owner.address)
+        XCTAssertNil(viewModel.imageURL)
+    }
+}
