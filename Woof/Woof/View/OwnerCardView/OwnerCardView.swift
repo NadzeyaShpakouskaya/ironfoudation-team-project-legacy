171e07c6ad0dfe8e915e5433e7b67a3367db3ed2
Julya Kmita <>
2023-06-01 17:24:53 +0800

    Cgange file Woof/Woof/View/OwnerCardView/OwnerCardView.swift

--- Woof/Woof/View/OwnerCardView/OwnerCardView.swift
+++ Woof/Woof/View/OwnerCardView/OwnerCardView.swift
@@ -2,42 +2,54 @@ import SwiftUI
 
 /// A view displaying information about the pet owner.
 struct OwnerCardView: View {
-    /// The view model responsible for providing data to the view.
-    @ObservedObject var viewModel: OwnerCardViewModel
+    /// The name of the pet owner.
+    let name: String
+    /// The surname of the owner
+    let surname: String
+    /// The phone of the owner
+    let phone: String
+    /// The address of the owner
+    let address: String
+    /// The URL of the avatar image of the owner.
+    let avatarUrl: URL?
 
     var body: some View {
         HStack {
-            AvatarView(url: viewModel.imageURL)
+            AvatarView(url: avatarUrl)
 
-            VStack(alignment: .leading, spacing: 8) {
-                Text(viewModel.fullName)
-                    .font(
-                        .system(
-                            size: AppStyle.FontStyle.heading.size, weight: .heavy
-                        ))
-                    .lineLimit(1)
-                    .foregroundColor(.App.white)
-                LabelWithTextAndImage(
-                    iconName: Image.IconName.phone,
-                    text: viewModel.phoneNumber
-                )
-                LabelWithTextAndImage(
-                    iconName: Image.IconName.house,
-                    text: viewModel.address
+            VStack(alignment: .leading, spacing: AppStyle.UIElementConstant.vStackSpacing) {
+                Group {
+                    Text(name)
+                    Text(surname)
+                }
+                .font(
+                    .system(
+                        size: AppStyle.FontStyle.heading.size,
+                        weight: .heavy
+                    ))
+                Group {
+                    Label(phone, systemImage: Image.IconName.phone)
+                    Label(address, systemImage: Image.IconName.house)
+                }
+                .font(
+                    .system(
+                        size: AppStyle.FontStyle.body.size,
+                        weight: .bold
+                    )
                 )
             }
             Spacer()
         }
         .padding()
         .background(Color.App.purpleLight)
+        .foregroundColor(.App.white)
         .cornerRadius(AppStyle.UIElementConstant.cornerRadius)
         .frame(maxWidth: .infinity)
     }
+}
 
-    struct OwnerCardView_Previews: PreviewProvider {
-        static var previews: some View {
-            OwnerCardView(
-                viewModel: OwnerCardViewModel(owner: Owner.Dummy.laraCroft))
-        }
+struct OwnerCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OwnerCardView(name: "sdf", surname: "dfdf", phone: "3435", address: "weg", avatarUrl: nil)
     }
 }
