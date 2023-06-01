7ee258a1f76de82526ec4590fd76af5e64111da7
Julya Kmita <>
2023-06-01 17:24:21 +0800

    Change file Woof/Woof/Model/PetOwner/PetOwner+Dummy.swift

--- Woof/Woof/Model/PetOwner/PetOwner+Dummy.swift
+++ Woof/Woof/Model/PetOwner/PetOwner+Dummy.swift
@@ -1,13 +1,14 @@
 import Foundation
+
 extension Owner {
     /// Provides dummy data for `PetOwner` model.
     enum Dummy {
-        /// The instance of dummy pet owner to use in preview.
+        /// The instance of dummy pet owner containing a static data in the fields.
         static let laraCroft = Owner(
             id: UUID(),
             name: "Lara",
             surname: "Croft",
-            phone: "802995349399",
+            phone: "80299534939",
             avatarUrl: Bundle.main.url(forResource: "EmilyDoeAvatar", withExtension: "jpg"),
             address: "My town, Happiness str. 24-67",
             rating: 4.8
