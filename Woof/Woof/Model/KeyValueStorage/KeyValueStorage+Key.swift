de5b56f4fd8cb19f220ddc8dd3e1def4c5280aca
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-06-06 10:14:14 +0400

    Add new storage names and keys for sitter and preferences

--- Woof/Woof/Model/KeyValueStorage/KeyValueStorage+Key.swift
+++ Woof/Woof/Model/KeyValueStorage/KeyValueStorage+Key.swift
@@ -4,7 +4,14 @@ extension KeyValueStorage {
     /// Provides the keys for storage.
     enum Key {
         /// The key associated with the data that contains information
-        /// about user using the app as owner.
+        /// about the user who is currently using the app as the owner.
         static let currentOwner = "owner"
+
+        /// The key associated with the data that contains information
+        /// about the user who is currently using the app as the sitter.
+        static let currentSitter = "sitter"
+
+        /// The key associated with the data that contains user preferences.
+        static let preferences = "preferences"
     }
 }
