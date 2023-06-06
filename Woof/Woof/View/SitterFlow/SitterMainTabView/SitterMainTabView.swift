aa4ee5089f9e33aa12626eaab452e489acbafdd7
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-06-06 09:25:25 +0400

    Create mainTabView for sitter

---
+++ Woof/Woof/View/SitterFlow/SitterMainTabView/SitterMainTabView.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct SitterMainTabView: View {
+    @State private var selection: Tab = .schedule
+
+    init() {
+        customizeTabBar()
+    }
+
+    var body: some View {
+        NavigationView {
+            TabView(selection: $selection) {
+                Group {
+                    Text("My schedule")
+                        .tabItem {
+                            Label("Schedule", systemImage: .IconName.scheduleTab)
+                        }
+                        .tag(Tab.schedule)
+
+                    Text("Walkings")
+                        .tabItem {
+                            Label("Walkings", systemImage: .IconName.walkingsTab)
+                        }
+                        .tag(Tab.walkings)
+
+                    Text("My profile")
+                        .tabItem {
+                            Label("Profile", systemImage: .IconName.profileTab)
+                        }
+                        .tag(Tab.profile)
+                }
+            }
+            .tint(Color.App.purpleDark)
+            .navigationTitle(selection.header)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+
+    /**
+     Customizes the appearance of the tab bar.
+     */
+    private func customizeTabBar() {
+        let tabBarAppearance = UITabBar.appearance()
+        tabBarAppearance.unselectedItemTintColor = UIColor(Color.App.grayDark)
+    }
+}
+
+struct SitterMainTabview_Previews: PreviewProvider {
+    static var previews: some View {
+        SitterMainTabView()
+    }
+}
