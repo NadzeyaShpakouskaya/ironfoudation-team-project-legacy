614e930779e3973051a9f6a0c35671f4a97dff57
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-06-08 13:56:13 +0200

    Adding method LoginVideModel to set current user role in the app

--- Woof/Woof/ViewModel/LoginViewModel.swift
+++ Woof/Woof/ViewModel/LoginViewModel.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 final class LoginViewModel: ObservableObject {
-    
+    @discardableResult func setUserRole(_ role: Role) -> Bool {
+        PreferencesHandler.set(userRole: role)
+    }
 }
