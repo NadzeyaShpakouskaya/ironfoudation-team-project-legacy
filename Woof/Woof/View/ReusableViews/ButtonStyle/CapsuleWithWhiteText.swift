57e702b9d4d60ee9cd1e7f0177043809e062ca1f
marinkman <marinkman@gmail.com>
2023-06-09 10:50:25 +0300

    create `CapsuleWithWhiteText` struct

---
+++ Woof/Woof/View/ReusableViews/ButtonStyle/CapsuleWithWhiteText.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+/// A type that applies standard interaction behavior and a custom appearance to all buttons within a view hierarchy.
+/// The button will have a capsule shape of the specified color.
+/// The button will have a label with the white center-aligned text.
+/// The button size will adjust to the length of the text.
+///
+/// To configure the current button style for a view hierarchy, use the `buttonStyle(_:)` modifier.
+struct CapsuleWithWhiteText: ButtonStyle {
+    /// A capsule color.
+    let capsuleColor: Color
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.horizontal)
+            .foregroundColor(Color.App.white)
+            .background(capsuleColor)
+            .clipShape(Capsule())
+    }
+}
+
+struct CapsuleWithWhiteText_Previews: PreviewProvider {
+    static var previews: some View {
+        Button("Press Me!") {}
+            .buttonStyle(
+                CapsuleWithWhiteText(
+                    capsuleColor: Color.App.purpleDark
+                )
+            )
+    }
+}
