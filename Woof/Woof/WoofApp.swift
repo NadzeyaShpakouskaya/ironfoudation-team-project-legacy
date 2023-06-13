502322e2fbfda16977ab4d1bc35001d9b911fc6f
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-06-13 15:11:00 +0400

    Update the WindowGroupScene based on the user role

--- Woof/Woof/WoofApp.swift
+++ Woof/Woof/WoofApp.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 @main
 struct WoofApp: App {
+    @StateObject var viewModel = StartScreenViewModel()
+
     var body: some Scene {
         WindowGroup {
-            OwnerMainTabView()
+            switch viewModel.userRole {
+            case .owner: OwnerMainTabView()
+            case .sitter: SitterMainTabView()
+            case .none: Text("Temporary sing in screen")
+            }
         }
     }
 }
