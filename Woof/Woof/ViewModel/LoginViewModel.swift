cd6f1889d2a1247e01d976b487a5731666371e18
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-06-13 12:47:11 +0200

    Adding empty test file and reworking doc comments

--- Woof/Woof/ViewModel/LoginViewModel.swift
+++ Woof/Woof/ViewModel/LoginViewModel.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-/// An interface is responsible for preparing and providing data for the `LoginView`.
+/// An interface is responsible for changing current user role in the app for the `LoginView`.
 final class LoginViewModel: ObservableObject {
-    
+    /// Saves the selected role as a user preference.
     func setUserRole(_ role: Role) -> Bool {
         PreferencesHandler.set(userRole: role)
     }
