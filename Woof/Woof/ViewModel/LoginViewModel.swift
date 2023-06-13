546a7cf5a90a66a21cea83cfc59310a31ace3129
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-06-13 14:04:54 +0200

    Fix doc comment for viewmodel class

--- Woof/Woof/ViewModel/LoginViewModel.swift
+++ Woof/Woof/ViewModel/LoginViewModel.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// An interface is responsible for changing current user role in the app in the `LoginView`.
+/// An interface responsible for changing the current user role in the app on the `LoginView`.
 final class LoginViewModel: ObservableObject {
     /// Requests model layer to save selected user role.
     func set(_ userRole: Role) -> Bool {
