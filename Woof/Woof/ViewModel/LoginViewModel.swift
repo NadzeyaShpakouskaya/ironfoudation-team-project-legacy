627f233f158ec52256bc3b0e53a36145b845dc8c
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-06-13 12:02:24 +0200

    Refactoring viewmodel

--- Woof/Woof/ViewModel/LoginViewModel.swift
+++ Woof/Woof/ViewModel/LoginViewModel.swift
@@ -1,7 +1,9 @@
 import Foundation
 
+/// An interface is responsible for preparing and providing data for the `LoginView`.
 final class LoginViewModel: ObservableObject {
-    @discardableResult func setUserRole(_ role: Role) -> Bool {
+    
+    func setUserRole(_ role: Role) -> Bool {
         PreferencesHandler.set(userRole: role)
     }
 }
