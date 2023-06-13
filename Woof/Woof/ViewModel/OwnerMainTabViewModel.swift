188fc1b380a917f39e2f1b5d16fa3e8d5f07761d
Julya Kmita <>
2023-06-14 00:14:00 +0800

    Add modified file Woof/Woof/ViewModel/OwnerMainTabViewModel.swift

--- Woof/Woof/ViewModel/OwnerMainTabViewModel.swift
+++ Woof/Woof/ViewModel/OwnerMainTabViewModel.swift
@@ -2,8 +2,8 @@ import Foundation
 
 /// The view model for a owner main tab view, responsible for preparing and processing data for the view.
 final class OwnerMainTabViewModel: ObservableObject {
-    /// Resets the current owner role  in the app by default value `none`.
-    func resertCurrentOwnerRole() {
+    /// Resets the current owner role in the app by default value `none`.
+    func resetCurrentRole() {
         PreferencesHandler.set(userRole: .none)
     }
 }
