a33a45d1c904279109484b40eddfb36f13be6182
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-06-13 15:21:25 +0300

    fixed tests

--- Woof/WoofTests/Helper/DataTransformerTests/FiveStarRatingTests.swift
+++ Woof/WoofTests/Helper/DataTransformerTests/FiveStarRatingTests.swift
@@ -1,12 +1,36 @@
 import XCTest
 
 final class FiveStarRatingTests: XCTestCase {
-    func testReturnsExpectedStarsForTheGivenValues() {
+    func testReturnsCaseUnavailableWhenNegativeValueProvided() {
         XCTAssertEqual(DataTransformer.fiveStarRating(for: -25.0), .unavailable)
+    }
+
+    func testReturnsCaseUnavailableWhenZeroValueProvided() {
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 0), .unavailable)
+    }
+
+    func testReturnsCaseUnavailableWhenValueBetweenZeroAndOneHalfValueProvided() {
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 0.35), .unavailable)
-        XCTAssertEqual(DataTransformer.fiveStarRating(for: 0.5), .rated(.oneStar))
+    }
+
+    func testReturnsOneStarWhenValueBetweenOneHalfAndOneProvided() {
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 0.99), .rated(.oneStar))
+    }
+
+    func testReturnsFourStarsWhenValueBetweenFourAndFourAndHalfProvided() {
+        XCTAssertEqual(DataTransformer.fiveStarRating(for: 4.26), .rated(.fourStars))
+    }
+
+    func testReturnsFiveStarsWhenValueBetweenFourAndHalfAndFiveProvided() {
+        XCTAssertEqual(DataTransformer.fiveStarRating(for: 4.56), .rated(.fiveStars))
+    }
+
+    func testReturnsFiveStarsWhenValueMoreThenFiveProvided() {
+        XCTAssertEqual(DataTransformer.fiveStarRating(for: 124.56), .rated(.fiveStars))
+    }
+
+    func testReturnsExpectedStarsForTheGivenValues() {
+        XCTAssertEqual(DataTransformer.fiveStarRating(for: 0.5), .rated(.oneStar))
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 1.0), .rated(.oneStar))
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 1.5), .rated(.twoStars))
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 2.0), .rated(.twoStars))
@@ -16,6 +40,5 @@ final class FiveStarRatingTests: XCTestCase {
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 4.0), .rated(.fourStars))
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 4.5), .rated(.fiveStars))
         XCTAssertEqual(DataTransformer.fiveStarRating(for: 5.0), .rated(.fiveStars))
-        XCTAssertEqual(DataTransformer.fiveStarRating(for: 124.56), .rated(.fiveStars))
     }
 }
