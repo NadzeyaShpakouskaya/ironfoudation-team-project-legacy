0d64102233398f7284555206dc2500c2ada3aef5
Julya Kmita <>
2023-06-14 15:55:17 +0800

    Add modified file Woof/Woof/ViewModel/SitterMainTabViewModel.swift

--- Woof/Woof/ViewModel/SitterMainTabViewModel.swift
+++ Woof/Woof/ViewModel/SitterMainTabViewModel.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// The view model for a pet sitter main tab view, responsible for preparing and processing data for the view.
 final class SitterMainTabViewModel: ObservableObject {
-    /// Resets the current sitter role in the app by default value `none`.
+    /// Resets the current user role in the app to the default value.
     func resetCurrentRole() {
         PreferencesHandler.set(userRole: .none)
     }
