2291d80754fc4ddd7c76e05f5b0e21f133902573
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-06-14 10:36:34 +0400

    Fix: Add missing doc comments, remove  unnecessary default value for userRole

--- Woof/Woof/ViewModel/StartScreenViewModel.swift
+++ Woof/Woof/ViewModel/StartScreenViewModel.swift
@@ -2,9 +2,10 @@ import Foundation
 
 /// Responsible for managing the data and behavior of the start screen view.
 final class StartScreenViewModel: ObservableObject {
-    /// The user role that defines the app flow, it's `none` by default.
-    @Published var userRole: Role = .none
+    /// The user role that defines the app flow.
+    @Published var userRole: Role
 
+    /// Initializes a new instance of the `StartScreenViewModel`.
     init() {
         userRole = PreferencesHandler.getUserRole()
     }
