bf7310d8c25fbe0f06651c70b7e3c653d35eafe7
Julya Kmita <>
2023-06-15 12:13:34 +0800

    Add file Woof/Woof.xcodeproj/project.pbxproj

---
+++ Woof/Woof/View/ReusableViews/TextEditorWithPlaceholder.swift
@@ -0,0 +1,20 @@
+//
+//  TextEditorWithPlaceholder.swift
+//  Woof
+//
+//  Created by Julya Kmita on 15/6/23.
+//
+
+import SwiftUI
+
+struct TextEditorWithPlaceholder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextEditorWithPlaceholder_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditorWithPlaceholder()
+    }
+}
---
+++ Woof/Woof/View/SitterFlow/EditSitterInformationView.swift
@@ -0,0 +1,20 @@
+//
+//  EditSitterInformationView.swift
+//  Woof
+//
+//  Created by Julya Kmita on 15/6/23.
+//
+
+import SwiftUI
+
+struct EditSitterInformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditSitterInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditSitterInformationView()
+    }
+}
