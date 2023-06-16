28642803bc0047fcd7853fd7ca1cbfc3bd9cc73f
Julya Kmita <>
2023-06-16 18:49:13 +0800

    Add file Woof/WoofTests/ViewModel/SitterProfileViewModelTests/SitterProfileViewModelSaveTests.swift

--- Woof/WoofTests/ViewModel/SitterProfileViewModelTests/SitterProfileViewModelSaveTests.swift
+++ Woof/WoofTests/ViewModel/SitterProfileViewModelTests/SitterProfileViewModelSaveTests.swift
@@ -1,8 +1,33 @@
-//
-//  SitterProfileViewModelSaveTests.swift
-//  Woof
-//
-//  Created by Julya Kmita on 16/6/23.
-//
+import XCTest
 
-import Foundation
+final class SitterProfileViewModelSaveTests: XCTestCase {
+    func testSaveMethodExistsInAPI() {
+        SitterProfileViewModel().save()
+    }
+
+    func testSaveMethodSuccessfullySavedModifiedSitterProperties() {
+        let viewModel = SitterProfileViewModel()
+
+        let newName = Sitter.Test.johnSmith.name
+        let newSurname = Sitter.Test.johnSmith.surname
+        let newPhone = Sitter.Test.johnSmith.phone
+        let newBio = Sitter.Test.johnSmith.bio
+        let newPricePerHour = Sitter.Test.johnSmith.pricePerHour
+
+        viewModel.name = newName
+        viewModel.surname = newSurname
+        viewModel.phone = newPhone
+        viewModel.bio = newBio
+        viewModel.pricePerHour = newPricePerHour
+
+        viewModel.save()
+
+        let loadedSitter = viewModel.getCurrentSitter()
+
+        XCTAssertEqual(loadedSitter.name, newName)
+        XCTAssertEqual(loadedSitter.surname, newSurname)
+        XCTAssertEqual(loadedSitter.phone, newPhone)
+        XCTAssertEqual(loadedSitter.bio, newBio)
+        XCTAssertEqual(loadedSitter.pricePerHour, newPricePerHour)
+    }
+}
