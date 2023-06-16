8a6527911569f469d384dc3b5bf0ca61131abe92
Julya Kmita <>
2023-06-16 18:46:22 +0800

    Add init() to the Woof/Woof/Model/Sitter/Sitter.swift

--- Woof/Woof/Model/Sitter/Sitter.swift
+++ Woof/Woof/Model/Sitter/Sitter.swift
@@ -20,7 +20,7 @@ struct Sitter: Codable, Identifiable {
     /// The URL of the sitter's avatar image.
     var avatarUrl: URL?
 
-    /// The additional information about the sitter, like his experience with dogs, favorite places for walks,
+    /// The additional information about the sitter, like his experience with dogs, favourite places for walks,
     /// special skills, certificates, etc.
     var bio: String
 
@@ -29,4 +29,61 @@ struct Sitter: Codable, Identifiable {
 
     /// The price per hour charged by the sitter.
     var pricePerHour: Double
+    /**
+     Creates an pet sitter instance with the specified parameters with the unique identifier.
+
+     - Parameters:
+     - name: The first name of the pet sitter.
+     - surname: The last name of the pet sitter.
+     - phone: The phone number of the pet sitter.
+     - avatarURL: The URL that provides an avatar image of the pet sitter.
+     - bio: The additional information about the pet sitter.
+     - rating: The rating of the pet sitter.
+     - pricePerHour: The price per hour charged by the pet sitter.
+
+     Each instance of the new pet sitter has a unique id.
+     */
+    init(
+        name: String,
+        surname: String,
+        phone: String,
+        avatarUrl: URL?,
+        bio: String,
+        rating: Double,
+        pricePerHour: Double
+    ) {
+        id = UUID()
+        self.name = name
+        self.surname = surname
+        self.phone = phone
+        self.avatarUrl = avatarUrl
+        self.bio = bio
+        self.rating = rating
+        self.pricePerHour = pricePerHour
+    }
+
+    /**
+     Creates a new pet sitter instance with the specified parameters described below.
+
+     The parameters of a new initialised instance are:
+     - `name` is hardcoded value `User`;
+     - `avatarUrl` is nil;
+     - `rating` is equal 0;
+     - `pricePerHour`is equal 0;
+     - other parameters are specified with the empty string ("").
+
+     Each instance of the new pet sitter has a unique id.
+     */
+
+    init() {
+        self.init(
+            name: "User",
+            surname: "",
+            phone: "",
+            avatarUrl: nil,
+            bio: "",
+            rating: 0,
+            pricePerHour: 0
+        )
+    }
 }
