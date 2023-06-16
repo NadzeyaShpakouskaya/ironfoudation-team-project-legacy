cb36ed570cb615f7e9aab2abdf72bf92cbd28769
Julya Kmita <>
2023-06-16 18:51:08 +0800

    Add file Woof/WoofTests/ViewModel/SitterProfileViewModelTests/Sitter+Tests.swift

--- Woof/WoofTests/ViewModel/SitterProfileViewModelTests/Sitter+Tests.swift
+++ Woof/WoofTests/ViewModel/SitterProfileViewModelTests/Sitter+Tests.swift
@@ -1,8 +1,17 @@
-//
-//  Sitter+Tests.swift
-//  Woof
-//
-//  Created by Julya Kmita on 16/6/23.
-//
-
 import Foundation
+
+extension Sitter {
+    /// Data used in unit tests.
+    enum Test {
+        /// The instance of the dummy sitter.
+        static let johnSmith = Sitter(
+            name: "John",
+            surname: "Smith",
+            phone: "1234567890",
+            avatarUrl: Bundle.main.url(forResource: "JohnAlisonAvatar", withExtension: "jpg"),
+            bio: "Experienced dog lover. Loves taking dogs for long walks in the park.",
+            rating: 4.0,
+            pricePerHour: 5.2
+        )
+    }
+}
