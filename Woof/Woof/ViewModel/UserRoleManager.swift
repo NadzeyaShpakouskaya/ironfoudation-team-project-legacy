0a1157e24eae80545098c04211b52ce713ec1e1b
marinkman <marinkman@gmail.com>
2023-06-18 17:00:42 +0300

    minor fix

--- Woof/Woof/ViewModel/UserRoleManager.swift
+++ Woof/Woof/ViewModel/UserRoleManager.swift
@@ -3,7 +3,11 @@ import Foundation
 /// Responsible for managing and actual state of the current user role in the application.
 final class UserRoleManager: ObservableObject {
     /// The current user role that defines the app flow.
-    @Published var userRole: Role
+    @Published var userRole: Role {
+        didSet {
+            PreferencesHandler.set(userRole: userRole)
+        }
+    }
 
     /// Initializes a new instance of the `UserRoleManager`.
     init() {
@@ -13,18 +17,15 @@ final class UserRoleManager: ObservableObject {
     /// Resets the current user role in the app to the default value.
     func resetCurrentRole() {
         userRole = .none
-        PreferencesHandler.set(userRole: userRole)
     }
 
     /// Sets the owner role in the application.
     func setOwnerRole() {
         userRole = .owner
-        PreferencesHandler.set(userRole: userRole)
     }
 
     /// Sets the sitter role in the application.
     func setSitterRole() {
         userRole = .sitter
-        PreferencesHandler.set(userRole: userRole)
     }
 }
