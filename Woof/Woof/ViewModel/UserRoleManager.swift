2e76ea5d614445d745e4ae91235c0d74fafe9690
marinkman <marinkman@gmail.com>
2023-06-18 15:36:41 +0300

    add methods to the `UserRoleManager`

--- Woof/Woof/ViewModel/UserRoleManager.swift
+++ Woof/Woof/ViewModel/UserRoleManager.swift
@@ -1,12 +1,30 @@
 import Foundation
 
-/// Responsible for managing the data and behavior of the start screen view.
+/// Responsible for managing and actual state of the current user role in the application.
 final class UserRoleManager: ObservableObject {
-    /// The user role that defines the app flow.
+    /// The current user role that defines the app flow.
     @Published var userRole: Role
 
     /// Initializes a new instance of the `UserRoleManager`.
     init() {
         userRole = PreferencesHandler.getUserRole()
+    }
+
+    /// Resets the current user role in the app to the default value.
+    func resetCurrentRole() {
+        userRole = .none
+        PreferencesHandler.set(userRole: userRole)
+    }
+
+    /// Sets the owner role in the application.
+    func setOwnerRole() {
+        userRole = .owner
+        PreferencesHandler.set(userRole: userRole)
+    }
+
+    /// Sets the sitter role in the application.
+    func setSitterRole() {
+        userRole = .sitter
+        PreferencesHandler.set(userRole: userRole)
     }
 }
