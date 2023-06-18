ca4c85e1f2599f8ef5ece256440289271b6f1ad7
marinkman <marinkman@gmail.com>
2023-06-18 15:44:18 +0300

    create `UserRoleManagerSetOwnerRoleTests` class

---
+++ Woof/WoofTests/ViewModel/UserRoleManagerTests/UserRoleManagerSetOwnerRoleTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+
+final class UserRoleManagerSetOwnerRoleTests: XCTestCase {
+    private var userPreferencesStorage = KeyValueStorage.testPreferencesStorage
+    private var userRoleManager = UserRoleManager()
+
+    override func setUp() {
+        super.setUp()
+        userPreferencesStorage.deleteData(for: KeyValueStorage.Key.userPreferences)
+    }
+
+    func testMethodExistsInAPI() {
+        userRoleManager.setOwnerRole()
+    }
+
+    func testMethodSetsTheOwnerRole() {
+        // Given
+        userRoleManager.resetCurrentRole()
+
+        // When
+        userRoleManager.setOwnerRole()
+
+        // Then
+        XCTAssertEqual(userRoleManager.userRole, .owner)
+    }
+
+    func testMethodOverridesTheSetSitterRole() {
+        // Given
+        userRoleManager.setSitterRole()
+
+        // When
+        userRoleManager.setOwnerRole()
+
+        // Then
+        XCTAssertEqual(userRoleManager.userRole, .owner)
+    }
+}
