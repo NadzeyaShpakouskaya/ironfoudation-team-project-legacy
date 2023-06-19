eb48e1b66ab83a42c8d61704901ba0eb6f917f7e
marinkman <marinkman@gmail.com>
2023-06-19 12:10:58 +0300

    update `OwnerMainTabViewModel`

--- Woof/Woof/ViewModel/SitterMainTabViewModel.swift
+++ Woof/Woof/ViewModel/SitterMainTabViewModel.swift
@@ -2,7 +2,6 @@ import Foundation
 
 /// The view model for a pet sitter main tab view, responsible for preparing and processing data for the view.
 final class SitterMainTabViewModel: ObservableObject {
-
     /// Indicates whether the alert should be shown.
     @Published var isAlertShown = false
 
