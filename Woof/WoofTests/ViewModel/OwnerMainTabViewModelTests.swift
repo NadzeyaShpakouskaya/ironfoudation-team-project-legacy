bf101b13f1b50facee9d3ec22822c93ef5156cd8
marinkman <marinkman@gmail.com>
2023-06-19 12:23:28 +0300

    update `OwnerMainTabViewModelTests`

--- Woof/WoofTests/ViewModel/OwnerMainTabViewModelTests.swift
+++ Woof/WoofTests/ViewModel/OwnerMainTabViewModelTests.swift
@@ -1,18 +1,56 @@
 import XCTest
 
 final class OwnerMainTabViewModelTests: XCTestCase {
-    func testResetCurrentRoleMethodExistsInAPI() {
-        OwnerMainTabViewModel().resetCurrentRole()
+    override func setUp() {
+        super.setUp()
+        viewModel = OwnerMainTabViewModel()
     }
 
-    func testResetCurrentRoleChangesUserRoleToNone() {
-        // Given
-        PreferencesHandler.set(userRole: .owner)
+    func testIsAlertShownPropertyExistsInAPI() {
+        _ = viewModel.isAlertShown
+    }
 
-        // When
-        OwnerMainTabViewModel().resetCurrentRole()
+    func testIsAlertShownPropertyReturnsAssignedValue() {
+        // Given // When
+        viewModel.isAlertShown = true
 
         // Then
-        XCTAssertEqual(PreferencesHandler.getUserRole(), Role.none)
+        XCTAssertTrue(viewModel.isAlertShown)
+
+        // Given // When
+        viewModel.isAlertShown = false
+
+        // Then
+        XCTAssertFalse(viewModel.isAlertShown)
     }
+
+    func testIsAlertShownPropertyReturnsFalseByDefault() {
+        XCTAssertFalse(viewModel.isAlertShown)
+    }
+
+    func testIsLogoutConfirmedPropertyExistsInAPI() {
+        _ = viewModel.isLogoutConfirmed
+    }
+
+    func testIsLogoutConfirmedPropertyReturnsAssignedValue() {
+        // Given // When
+        viewModel.isLogoutConfirmed = true
+
+        // Then
+        XCTAssertTrue(viewModel.isLogoutConfirmed)
+
+        // Given // When
+        viewModel.isLogoutConfirmed = false
+
+        // Then
+        XCTAssertFalse(viewModel.isLogoutConfirmed)
+    }
+
+    func testIsLogoutConfirmedPropertyReturnsFalseByDefault() {
+        XCTAssertFalse(viewModel.isLogoutConfirmed)
+    }
+
+    // MARK: - Private interface
+
+    private var viewModel = OwnerMainTabViewModel()
 }
