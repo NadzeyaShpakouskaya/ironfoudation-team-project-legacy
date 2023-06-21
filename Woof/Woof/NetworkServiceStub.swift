9674e3dce5d294ba2cdf0c63f1f2cf195eae9314
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-06-21 10:37:33 +0400

    Add the NetworkServiceStub

---
+++ Woof/Woof/NetworkServiceStub.swift
@@ -0,0 +1,9 @@
+import NetworkService
+
+/// Stub network service to check the integration of `NetworkService` package.
+struct NetworkServiceStub {
+    /// Test method to check the proper integration of `NetworkService` package.
+    func foo() {
+        NetworkService().testUse()
+    }
+}
