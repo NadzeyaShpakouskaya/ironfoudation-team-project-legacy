1f768b474a426bacfbcfd4f333fc1aa6914319e1
marinkman <marinkman@gmail.com>
2023-06-21 09:27:28 +0300

    add private methods to the `LoginViewModel` to load the owner and sitter from the storage

--- Woof/Woof/ViewModel/LoginViewModel.swift
+++ Woof/Woof/ViewModel/LoginViewModel.swift
@@ -9,4 +9,31 @@ final class LoginViewModel: ObservableObject {
 
     /// Indicates whether the sitter role is selected.
     @Published var isTheSitterRoleSelected = false
+    
+    // MARK: - Private interface
+
+    private func loadOwnerFromStorage() -> Owner? {
+        guard let data = KeyValueStorage(KeyValueStorage.Name.currentOwner)
+            .loadData(for: KeyValueStorage.Key.currentOwner) else {
+            return nil
+        }
+        guard let owner = try? JSONDecoder().decode(Owner.self, from: data) else {
+            return nil
+        }
+
+        return owner
+    }
+
+    private func loadSitterFromStorage() -> Sitter? {
+        guard let data = KeyValueStorage(KeyValueStorage.Name.currentSitter)
+            .loadData(for: KeyValueStorage.Key.currentSitter) else {
+            return nil
+        }
+        guard let sitter = try? JSONDecoder().decode(Sitter.self, from: data) else {
+            return nil
+        }
+
+        return sitter
+    }
+
 }
