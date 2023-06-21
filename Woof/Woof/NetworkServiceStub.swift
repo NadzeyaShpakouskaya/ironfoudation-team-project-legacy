f099990fdef570046534d0e53a2f1bc9da5ba691
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-06-21 13:08:18 +0400

    Fix doc comments for method

--- Woof/Woof/NetworkServiceStub.swift
+++ Woof/Woof/NetworkServiceStub.swift
@@ -2,7 +2,7 @@ import NetworkService
 
 /// Stub network service to check the integration of `NetworkService` package.
 struct NetworkServiceStub {
-    /// Test method to check the proper integration of `NetworkService` package.
+    /// Check the proper integration of `NetworkService` package.
     func foo() {
         NetworkService().testUse()
     }
