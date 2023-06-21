ecc37e88f92e7110ab5ffd3a38e860c1731972b0
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-06-21 20:24:59 +0400

    Move testing of package integration to test target

--- Woof/Woof/NetworkServiceStub.swift
+++
@@ -1,9 +0,0 @@
-import NetworkService
-
-/// Stub network service to check the integration of `NetworkService` package.
-struct NetworkServiceStub {
-    /// Check the proper integration of `NetworkService` package.
-    func foo() {
-        NetworkService().testUse()
-    }
-}
