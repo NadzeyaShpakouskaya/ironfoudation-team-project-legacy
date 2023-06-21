452cd2a07c47013fd322930ebdc89653bab8adf1
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-06-21 15:07:34 +0200

    Fixing build script

--- scripts/build-ios-simulator.sh
+++ scripts/build-ios-simulator.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-APP_NAME=“Woof”
-xcodebuild -scheme NetworkService
+APP_NAME=Woof
+xcodebuild -project Woof/${APP_NAME}.xcodeproj \
+           -scheme NetworkService
 xcodebuild -project Woof/${APP_NAME}.xcodeproj \
            -scheme ${APP_NAME} \
            -configuration Release \
