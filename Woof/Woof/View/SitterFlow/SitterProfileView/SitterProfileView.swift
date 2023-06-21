e2efeb8a52f8418dd10b11fa169fd80bce0b543c
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-06-21 10:45:36 +0300

    add file `SitterProfileView` and add edit view to the Profile folder

---
+++ Woof/Woof/View/SitterFlow/SitterProfileView/SitterProfileView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SitterProfileView: View {
+    var body: some View {
+        Text("Sitter Profile")
+    }
+}
+
+struct SitterProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        SitterProfileView()
+    }
+}
