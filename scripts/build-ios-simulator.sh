aa94e95c47e5a82fb951ba35bc4509db28d11bdc
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-06-21 14:38:23 +0200

    Changing build target to a scheme

--- scripts/build-ios-simulator.sh
+++ scripts/build-ios-simulator.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-APP_NAME="Woof"
+APP_NAME=“Woof”
 xcodebuild -scheme NetworkService
 xcodebuild -project Woof/${APP_NAME}.xcodeproj \
-           -target ${APP_NAME} \
+           -scheme ${APP_NAME} \
            -configuration Release \
            -sdk iphonesimulator16.4
