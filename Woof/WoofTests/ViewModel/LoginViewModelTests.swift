bb8ad07a5d643499c6ef5270ae95bf3b7fc433bb
marinkman <marinkman@gmail.com>
2023-06-22 14:22:56 +0300

    add test methods

--- Woof/WoofTests/ViewModel/LoginViewModelTests.swift
+++ Woof/WoofTests/ViewModel/LoginViewModelTests.swift
@@ -1,16 +1,94 @@
 import XCTest
 
 final class LoginViewModelTests: XCTestCase {
+    private var viewModel = LoginViewModel()
+
+    override func setUp() {
+        super.setUp()
+        viewModel = LoginViewModel()
+
+        KeyValueStorage(KeyValueStorage.Name.currentOwner)
+            .deleteData(for: KeyValueStorage.Key.currentOwner)
+        KeyValueStorage(KeyValueStorage.Name.currentSitter)
+            .deleteData(for: KeyValueStorage.Key.currentSitter)
+    }
+
     func testLoginViewModelExistInAPI() {
-        _ = LoginViewModel()
+        _ = viewModel
     }
 
     func testLoginViewModelIsInitializedWithExpectedValues() {
-        // Given // When
-        let viewModel = LoginViewModel()
+        XCTAssertFalse(viewModel.isTheOwnerRoleSelected)
+        XCTAssertFalse(viewModel.isTheSitterRoleSelected)
+    }
+
+    func testWhenIsTheOwnerRoleSelectedIsTrueTheNewOwnerIsCreatedIfTheCurrentOwnerDoesNotExist() {
+        // Given
+        let currentOwner = KeyValueStorage(KeyValueStorage.Name.currentOwner)
+            .loadData(for: KeyValueStorage.Key.currentOwner)
+
+        // When
+        viewModel.isTheOwnerRoleSelected = true
+
+        let newOwner = KeyValueStorage(KeyValueStorage.Name.currentOwner)
+            .loadData(for: KeyValueStorage.Key.currentOwner)
 
         // Then
-        XCTAssertEqual(viewModel.isTheOwnerRoleSelected, false)
-        XCTAssertEqual(viewModel.isTheSitterRoleSelected, false)
+        XCTAssertNil(currentOwner)
+        XCTAssertNotNil(newOwner)
+    }
+
+    func testWhenIsTheOwnerRoleSelectedIsTrueNoNewOwnerIsCreatedIfTheCurrentOwnerAlreadyExist() {
+        // Given
+        viewModel.isTheOwnerRoleSelected = true
+
+        let currentOwner = KeyValueStorage(KeyValueStorage.Name.currentOwner)
+            .loadData(for: KeyValueStorage.Key.currentOwner)
+
+        // When
+        let newViewModel = LoginViewModel()
+
+        newViewModel.isTheOwnerRoleSelected = true
+
+        let newOwner = KeyValueStorage(KeyValueStorage.Name.currentOwner)
+            .loadData(for: KeyValueStorage.Key.currentOwner)
+
+        // Then
+        XCTAssertEqual(currentOwner, newOwner)
+    }
+
+    func testWhenIsTheSitterRoleSelectedIsTrueTheNewSitterIsCreatedIfTheCurrentSitterDoesNotExist() {
+        // Given
+        let currentSitter = KeyValueStorage(KeyValueStorage.Name.currentSitter)
+            .loadData(for: KeyValueStorage.Key.currentSitter)
+
+        // When
+        viewModel.isTheSitterRoleSelected = true
+
+        let newSitter = KeyValueStorage(KeyValueStorage.Name.currentSitter)
+            .loadData(for: KeyValueStorage.Key.currentSitter)
+
+        // Then
+        XCTAssertNil(currentSitter)
+        XCTAssertNotNil(newSitter)
+    }
+
+    func testWhenIsTheSitterRoleSelectedIsTrueNoNewSitterIsCreatedIfTheCurrentSitterAlreadyExist() {
+        // Given
+        viewModel.isTheSitterRoleSelected = true
+
+        let currentSitter = KeyValueStorage(KeyValueStorage.Name.currentSitter)
+            .loadData(for: KeyValueStorage.Key.currentSitter)
+
+        // When
+        let newViewModel = LoginViewModel()
+
+        newViewModel.isTheSitterRoleSelected = true
+
+        let newSitter = KeyValueStorage(KeyValueStorage.Name.currentSitter)
+            .loadData(for: KeyValueStorage.Key.currentSitter)
+
+        // Then
+        XCTAssertEqual(currentSitter, newSitter)
     }
 }
