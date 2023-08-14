177e505fd360333385fc393278ef88305cc2dd92
kirpichonok <ivankirpichonok@gmail.com>
2023-08-14 16:17:24 +0300

    Update Obfuscator: rewrite implementation and add documentation.

--- Woof/Woof/Helper/Obfuscator/Obfuscator.swift
+++ Woof/Woof/Helper/Obfuscator/Obfuscator.swift
@@ -1,67 +1,40 @@
-import Foundation
-
-class Obfuscator {
+enum Obfuscator {
     // MARK: - Internal interface
 
-    init() {
-        salt = "\(String(describing: UserRoleViewModel.self))\(String(describing: NSObject.self))"
-    }
-
-    /**
-     Takes a string and obfuscates it by XOR-ing it with a cipher.
-
-     - Parameter string: The string to be obfuscated.
-
-     - Returns: An array of UInt8 bytes representing the obfuscated string.
-     */
-    func bytesByObfuscatingString(string: String) -> [UInt8] {
-        let text = [UInt8](string.utf8)
-        let cipher = [UInt8](salt.utf8)
-        let length = cipher.count
-
-        var encrypted = [UInt8]()
-
-        for symbol in text.enumerated() {
-            encrypted.append(symbol.element ^ cipher[symbol.offset % length])
+    /// Reveals the original string by deobfuscating it using the cipher.
+    /// The passed encrypted string must be obfuscated with the same cipher as the function uses and
+    /// consist of ASCII representation of UInt8 numbers in base 10 separated by space.
+    ///
+    /// - Parameter key: The encrypted string value presented as a string with a sequence of ASCII
+    /// representations of UInt8 numbers in base 10 separated by space.
+    /// - Returns: The original string revealed by deobfuscating the obfuscated bytes.
+    /// - Throws: An `ObfuscatorError` if obfuscation was not successful.
+    /// - Complexity: O(*N*), where *N* is the length of the input string.
+    static func reveal(key: String) throws -> String {
+        let keyAsBytes = try key.split(separator: " ").compactMap { stringUInt8 in
+            guard let byte = UInt8(stringUInt8) else { throw ObfuscatorError.nonInt8Value }
+            return byte
         }
 
-        #if DEVELOPMENT
-            print("Salt used: \(salt)\n")
-            print("Swift Code:\n************")
-            print("// Original \"\(string)\"")
-            print("let key: [UInt8] = \(encrypted)\n")
-        #endif
+        guard !keyAsBytes.isEmpty else { return "" }
 
-        return encrypted
-    }
-
-    /**
-     Takes an array of obfuscated UInt8 bytes and reveals the original string by deobfuscating it using the cipher.
-     The cipher must be the same as the one used to encrypt it in the first place.
-
-     - Parameter key: An array of obfuscated UInt8 bytes to decrypted.
-
-     - Returns: The original string revealed by deobfuscating the obfuscated bytes.
-     */
-    func reveal(key: [UInt8]) -> String? {
         let cipher = [UInt8](salt.utf8)
         let length = cipher.count
 
         var decrypted = [UInt8]()
 
-        for symbol in key.enumerated() {
-            decrypted.append(symbol.element ^ cipher[symbol.offset % length])
+        for (index, symbol) in keyAsBytes.enumerated() {
+            decrypted.append(symbol ^ cipher[index % length])
         }
 
-        if let revealedData = String(bytes: decrypted, encoding: .utf8) {
-            return revealedData
-        } else {
-            return nil
+        guard let result = String(bytes: decrypted, encoding: .utf8) else {
+            throw ObfuscatorError.unconvertibleToString
         }
+
+        return result
     }
 
     // MARK: - Private interface
 
-    /// The salt used to obfuscate and reveal the string.
-    private var salt: String
+    private static var salt = ""
 }
