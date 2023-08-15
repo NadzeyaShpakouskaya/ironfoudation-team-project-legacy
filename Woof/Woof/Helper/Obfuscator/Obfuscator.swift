322f1ead55628ae0eac123deb2f3d5e67db462db
kirpichonok <ivankirpichonok@gmail.com>
2023-08-15 11:49:25 +0300

    Update Obfuscator: rename variables, add salt as parameter.

--- Woof/Woof/Helper/Obfuscator/Obfuscator.swift
+++ Woof/Woof/Helper/Obfuscator/Obfuscator.swift
@@ -5,36 +5,34 @@ enum Obfuscator {
     /// The passed encrypted string must be obfuscated with the same cipher as the function uses and
     /// consist of ASCII representation of UInt8 numbers in base 10 separated by space.
     ///
-    /// - Parameter key: The encrypted string value presented as a string with a sequence of ASCII
+    /// - Parameters:
+    ///  - key: The encrypted string value presented as a string with a sequence of ASCII
     /// representations of UInt8 numbers in base 10 separated by space.
+    ///  - salt: The salt used to obfuscate and reveal the string.
     /// - Returns: The original string revealed by deobfuscating the obfuscated bytes.
     /// - Throws: An `ObfuscatorError` if obfuscation was not successful.
     /// - Complexity: O(*N*), where *N* is the length of the input string.
-    static func reveal(key: String) throws -> String {
-        let keyAsBytes = try key.split(separator: " ").compactMap { stringUInt8 in
-            guard let byte = UInt8(stringUInt8) else { throw ObfuscatorError.nonInt8Value }
+    static func reveal(key: String, salt: String) throws -> String {
+        let keyAsSequenceOfBytes = try key.split(separator: " ").map { stringRepresentationOfUInt8 in
+            guard let byte = UInt8(stringRepresentationOfUInt8) else { throw ObfuscatorError.nonInt8Value }
             return byte
         }
 
-        guard !keyAsBytes.isEmpty else { return "" }
+        guard !keyAsSequenceOfBytes.isEmpty else { return "" }
 
         let cipher = [UInt8](salt.utf8)
-        let length = cipher.count
+        let lengthOfCipher = cipher.count
 
-        var decrypted = [UInt8]()
+        var decryptedSequenceOfBytes = [UInt8]()
 
-        for (index, symbol) in keyAsBytes.enumerated() {
-            decrypted.append(symbol ^ cipher[index % length])
+        for (index, byte) in keyAsSequenceOfBytes.enumerated() {
+            decryptedSequenceOfBytes.append(byte ^ cipher[index % lengthOfCipher])
         }
 
-        guard let result = String(bytes: decrypted, encoding: .utf8) else {
+        guard let decryptedString = String(bytes: decryptedSequenceOfBytes, encoding: .utf8) else {
             throw ObfuscatorError.unconvertibleToString
         }
 
-        return result
+        return decryptedString
     }
-
-    // MARK: - Private interface
-
-    private static var salt = ""
 }
