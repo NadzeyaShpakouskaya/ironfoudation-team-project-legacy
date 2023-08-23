bba21b492dd7b329607e20e39e4e5a492c11cdcf
kirpichonok <ivankirpichonok@gmail.com>
2023-08-23 13:30:34 +0300

    Update obfuscator.sh: add comments.

--- scripts/obfuscator/obfuscator.sh
+++ scripts/obfuscator/obfuscator.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# Check if the number of command-line arguments is 0
 if [ $# -eq 0 ]; then
     echo "Please provide a string to obfuscate as an argument."
     exit 1
 fi
 
+# Check if the salt file exists
 if [ ! -f "./salt" ]; then
     echo "Salt file 'salt' not found. Please create it."
     exit 1
 fi
 
+# Read the salt from the salt file
 salt=$(< ./salt)
 
+# Obfuscate function
 obfuscate() {
     local string="$1"
     local salt="$2"
@@ -24,27 +28,32 @@ obfuscate() {
     local -a stringAsBytes
     local -a cipher
 
+    # Convert string characters to ASCII values
     for (( i = 0; i < ${#string}; i++ )); do
         stringAsBytes[$i]=$(printf "%d" "'${string:$i:1}")
     done
 
     if [ -z "$salt" ]; then
-        for byte in "${stringAsBytes[@]}"; do
-            echo -n "$byte "
-        done
+        # Print ASCII values if salt is empty
+        printf "%s " "${stringAsBytes[@]}"
         return
     fi
 
+    # Convert salt characters to ASCII values
     for (( i = 0; i < ${#salt}; i++ )); do
         cipher[$i]=$(printf "%d" "'${salt:$i:1}")
     done
     cipherLength=${#cipher[@]}
 
+    # XOR obfuscation
     for (( i = 0; i < ${#stringAsBytes[@]}; i++ )); do
         encrypted=$(( ${stringAsBytes[$i]} ^ ${cipher[$i % cipherLength]} ))
         echo -n "$encrypted "
     done
 }
 
+# Call the obfuscate function and store the result
 encryptedResult=$(obfuscate "$1" "$salt")
+
+# Write the encrypted result to the specified path
 echo "$encryptedResult" > "./api-key"
