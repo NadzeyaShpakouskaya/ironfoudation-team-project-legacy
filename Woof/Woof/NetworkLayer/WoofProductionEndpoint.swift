c2820b1af77daee4e319e3828b3d82de830e0010
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-08-24 08:04:56 +0400

    Add conformation to `HTTPAPIEndpoint` for `WoofProductionEndpoint`

--- Woof/Woof/NetworkLayer/WoofProductionEndpoint.swift
+++ Woof/Woof/NetworkLayer/WoofProductionEndpoint.swift
@@ -1,26 +1,64 @@
 import Foundation
+import NetworkService
 
 ///  Defines the production endpoints for the Woof app's API.
-enum WoofProductionEndpoint {
+enum WoofProductionEndpoint: HTTPAPIEndpoint {
+    typealias Environment = APIEnvironment
+
     // MARK: - Internal interface
 
-    /// The production base URL of the Woof API
-    static let baseURL = URL(string: "https://woof-app.hasura.app/api/rest/")!
+    /// Request to get a list of the available sitters from endpoint.
+    case getSitters
 
-    /// The authentication header used for making requests to the Woof API.
-    static var authHeader: String {
-        // obfuscate header
-        ""
+    /// Request to add or update, if the sitter already exists, information about sitter on the endpoint.
+    case addNew(sitter: Sitter)
+
+    static var networkEnvironment = Environment.production
+
+    var baseURL: URL {
+        Self.baseProdURL
     }
 
-    /// The key used for authentication with the Woof API.
-    static var key: String {
-        // obfuscate key
-        ""
+    var path: String {
+        switch self {
+        case .getSitters: return Self.getAllSittersPath
+        case .addNew(_): return Self.addNewSitterPath
+        }
+    }
+
+    var task: HTTPTask {
+        switch self {
+        case .getSitters: return .request
+        case let .addNew(sitter): return .requestWithBodyParameters(bodyParameters: ["\(sitter.id)": sitter])
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .getSitters: return .get
+        case .addNew(_): return .post
+        }
+    }
+
+    var headers: HTTPHeaders {
+        switch Self.networkEnvironment {
+        case .production: return [Self.authHeader: Self.authKey ?? ""]
+        case .staging: return [Self.authHeader: Self.authKey ?? ""]
+        }
     }
 
     // MARK: - Private interface
 
-    private static let obfuscateHeader: [UInt8] = []
-    private static let obfuscatedKey: [UInt8] = []
+    private static let baseProdURL = URL(string: "https://woof-app.hasura.app/api/rest/")!
+    private static let getAllSittersPath = "pet_sitters/all"
+    private static let addNewSitterPath = "pet_sitters/new"
+    private static let authHeader = "x-hasura-admin-secret"
+    private static let obfuscatedHeader = ""
+    private static let obfuscatedKey = ""
+    private static let authKey = try? Obfuscator.reveal("", salt: "")
+}
+
+enum APIEnvironment {
+    case production
+    case staging
 }
