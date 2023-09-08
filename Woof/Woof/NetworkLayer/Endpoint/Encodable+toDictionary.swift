a66f2c5c9852a2f95f87c2410e6995e071c208f5
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-09-08 09:55:56 +0400

    Add doc comments to extension to convert object to dictionary

--- Woof/Woof/NetworkLayer/Endpoint/Encodable+toDictionary.swift
+++ Woof/Woof/NetworkLayer/Endpoint/Encodable+toDictionary.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 extension Encodable {
+    /// Converts the object to a dictionary representation.
+    ///
+    /// - Throws: An error of type `Error` if encoding or serialization fails.
+    ///
+    /// - Returns: A dictionary representation of the encoded object.
     func asDictionary() throws -> [String: Any] {
         let data = try JSONEncoder().encode(self)
         let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
