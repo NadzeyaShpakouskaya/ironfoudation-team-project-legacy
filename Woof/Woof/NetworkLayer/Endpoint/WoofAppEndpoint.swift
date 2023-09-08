b546423ad08255bbe3b1fb0337db7c382f400613
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-09-08 11:00:39 +0400

    WoofAppEndpoint: add missing doc comments and tests

--- Woof/Woof/NetworkLayer/Endpoint/WoofAppEndpoint.swift
+++ Woof/Woof/NetworkLayer/Endpoint/WoofAppEndpoint.swift
@@ -1,4 +1,5 @@
 ///  Defines the production endpoints for the Woof app's API.
 enum WoofAppEndpoint {
+    /// Adding a new pet sitter.
     case addNewSitter(Sitter)
 }
---
+++ Woof/WoofTests/NetworkLayer/Endpoint/WoofAppEndpointTests.swift
@@ -0,0 +1,43 @@
+import NetworkService
+import XCTest
+
+final class WoofAppEndpointTests: XCTestCase {
+    func testAllWoofAppEndpointPropertiesAreAvailable() {
+        let _ = WoofAppEndpoint.networkEnvironment
+    }
+
+    func testAllCasesForWoofAppEndpointExists() {
+        let _ = WoofAppEndpoint.addNewSitter(Sitter())
+    }
+
+    func testAddNewSitterEndpointIsConfiguredCorrectly() {
+        // Given
+        let baseProdURL = URL(string: "https://woof-app.hasura.app/api/rest/") ?? Bundle.main.bundleURL
+        let authHeader = "x-hasura-admin-secret"
+
+        // When
+        let sitter = Sitter()
+
+        let endpoint = WoofAppEndpoint.addNewSitter(sitter)
+
+        let method = endpoint.method
+        let task = endpoint.task
+        let headers = endpoint.headers
+
+        // Then
+        XCTAssertEqual(endpoint.baseURL, baseProdURL)
+        XCTAssertEqual(endpoint.path, EndpointPath.addNewSitter)
+        XCTAssertTrue(headers.keys.contains(authHeader))
+
+        switch method {
+        case .post: break
+        default: XCTFail("Wrong method: expected `POST` method, but \(method) was settled")
+        }
+
+        switch task {
+        case .requestWithBodyParameters(_): break
+        default: XCTFail("Wrong task type: expected `requestWithBodyParameters` task, but \(task) was settled")
+        }
+    }
+}
+
