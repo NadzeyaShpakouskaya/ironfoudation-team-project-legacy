b2cb9e59e6c3b7131bc16c7b1ab42d30f95fd7da
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-09-13 09:30:47 +0400

    Update the docC for asDictionary method in Encodable extension

--- Woof/Woof/NetworkLayer/Endpoint/Encodable+asDictionary.swift
+++ Woof/Woof/NetworkLayer/Endpoint/Encodable+asDictionary.swift
@@ -1,10 +1,12 @@
 import Foundation
 
 extension Encodable {
-    /// Converts the object to a dictionary representation.
+    /// Converts the object to a dictionary representation, where keys
+    /// are the string representation of the stored properties' names and
+    /// values are the values of those properties.
+    /// During converting the camel case notation of properties' names replace with the snake case notation.
     ///
     /// - Throws: An error of type `Error` if encoding or serialization fails.
-    ///
     /// - Returns: A dictionary representation of the encoded object.
     func asDictionary() throws -> [String: Any] {
         let encoder = JSONEncoder()
