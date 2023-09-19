3c41f819e9a34f3e8971ac8cbb7f23e3edd50c49
kirpichonok <ivankirpichonok@gmail.com>
2023-09-19 13:47:15 +0300

    Update SitterListViewModel: add request to backend.

--- Woof/Woof/ViewModel/SitterListViewModel.swift
+++ Woof/Woof/ViewModel/SitterListViewModel.swift
@@ -1,12 +1,26 @@
 import Foundation
+import NetworkService
 
 /// An interface is responsible for preparing and providing data for the `SitterListView`.
 final class SitterListViewModel: ObservableObject {
+    // MARK: - Internal interface
+
     /// The list of sitters to show.
     @Published var sitters: [Sitter] = []
 
-    /// Initializes a new instance of the `SitterListViewModel`
-    init() {
-        sitters = Sitter.Dummy.sitters
+    func fetchSitters() async {
+        do {
+            let data = try await NetworkService().request(WoofAppEndpoint.getAllSitters)
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+            guard let decodedResponseData = try? decoder.decode([String: [Sitter]?].self, from: data),
+                  decodedResponseData.count == 1,
+                  let sitters = decodedResponseData.values.first as? [Sitter] else {
+                return
+            }
+
+            await MainActor.run { self.sitters = sitters }
+        } catch {}
     }
 }
