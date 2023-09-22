7981fb4ce89f045b97212d449b5b75c0919e22dd
kirpichonok <ivankirpichonok@gmail.com>
2023-09-22 21:54:16 +0300

    Update Sitter+Decoder

--- Woof/Woof/Model/Sitter/Sitter+Decoder.swift
+++ Woof/Woof/Model/Sitter/Sitter+Decoder.swift
@@ -7,7 +7,7 @@ extension Sitter {
         name = try container.decode(String.self, forKey: .name)
         surname = try container.decode(String.self, forKey: .surname)
         phone = (try? container.decode(String.self, forKey: .phone)) ?? ""
-        avatarUrl = try container.decode(URL?.self, forKey: .avatarUrl)
+        avatarUrl = try? container.decode(URL.self, forKey: .avatarUrl)
         bio = try container.decode(String.self, forKey: .bio)
         rating = (try? container.decode(Double.self, forKey: .rating)) ?? 0
         pricePerHour = (try? container.decode(Double.self, forKey: .pricePerHour)) ?? 0
